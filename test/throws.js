1331027568e2967db9855020f9de67993a8929b2
LasaleFamine <info@godev.space>
2017-03-05 15:15:05 +0100

    fix typo in test and added the `sortLatest` and `sortOldest` + tests #1

--- test/throws.js
+++ test/throws.js
@@ -12,6 +12,16 @@ test('throw when dir is not passed (oldest)', async t => {
 	t.is(err.message, 'Path must be a string. Received undefined');
 });
 
+test('throw when dir is not passed (sortLatest)', async t => {
+	const err = await t.throws(fn.sortLatest());
+	t.is(err.message, 'Path must be a string. Received undefined');
+});
+
+test('throw when dir is not passed (sortOldest)', async t => {
+	const err = await t.throws(fn.sortOldest());
+	t.is(err.message, 'Path must be a string. Received undefined');
+});
+
 test('throw when readdir throws', async t => {
 	const err = await t.throws(fn.latest('some/path/that/not/exist'));
 	t.is(err.message, `ENOENT: no such file or directory, scandir '${resolve('some/path/that/not/exist')}'`);
